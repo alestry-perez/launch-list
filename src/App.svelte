2e5d555c80fa5fbd338e0f9ad06faa5cf5b75c76
Alestry Pérez <alestry.perez@hyperisland.se>
2021-04-28 15:09:46 +0200

    added cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,10 @@
 <script>
+	import Card from './Card.svelte';
 	let title='Upcoming Launches';
-
-	let src = 'loading...';
 	
 	
 </script>
 
 <main>
-	<div class="flex items-center justify-center min-h-screen bg-gray-200">
-		<!-- Start of component -->
-		<div class="max-w-2xl p-3 tracking-wide bg-white border-2 border-gray-300 rounded-md shadow-lg">
-		   <div id="header" class="flex"> 
-			  <img alt="mountain" class="mt-3 bg-cover border-gray-300 max-w-7xl max-h-96 " src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.aeEolVHwf2538OOA_uWvUgHaPN%26pid%3DApi&f=1" />
-			  <div id="body" class="flex flex-col ml-5">
-				<div class="flex justify-end">
-					<span alt="avatar" class="flex items-center justify-center px-3 py-3 text-white bg-green-500 rounded-full" src="https://picsum.photos/seed/picsum/200">GO</span>
-					
-				 </div>
-				 <h4 id="name" class="mb-8 text-5xl font-semibold">Launch Title </h4>
-				 
-				 <div id="launchInformation" class='mb-5'>
-					 <h6 class='text-4xl'>Organization</h6>
-					 <h6 class='text-2xl'>Launch Pad Location</h6>
-				 </div>
-				 <div id="launchTime" class='mb-5'>
-					<h2 class="mt-2 text-gray-800 text-7xl">T 00:00:00</h2>
-				 </div>
-				 <div id='date' class='mb-8'>
-					 <h6 class='text-2xl'>Date: 01/01/2021</h6>
-					 <h6 class='text-2xl'>Time: 13:45hr</h6>
-				 </div>
-				 
-			  </div>
-		   </div>
-		</div>
-		<!-- End of component -->
-	 </div>
-	
+	<Card/>
 </main>
