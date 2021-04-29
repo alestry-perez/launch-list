164e9dddc8f16691a4dfbbc9354222b4957ef940
Alestry Pérez <alestry.perez@hyperisland.se>
2021-04-29 17:30:00 +0200

    imported API

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,24 @@
 <script>
+	import { onMount } from 'svelte'
 	import Card from './Card.svelte';
 	import NavBar from './NavBar.svelte';
 	import Footer from './Footer.svelte';
+	import { getData } from './launchAPI.js'
+	
+	let events =[];
+	$: console.log(events);
+
+	onMount(async()=>{
+		events = await getData();
+	})
 </script>
 
 <main class='bg-gray-200 select-auto'>
 	<NavBar/>
 	<div class="flex flex-col items-center justify-center">
-		
-		<Card 
-		launchTitle='Launch Title' 
-		organization='Organization Name' 
-		launchPadLocation='Pad Location' 
-		countdown='T 00:00:00'
-		date='01/01/2021'
-		time='13:45'
-		status='GO'
-		image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.aeEolVHwf2538OOA_uWvUgHaPN%26pid%3DApi&f=1'
-		></Card>
-		
+		{ #each events as {name, date} }
+			<Card />
+		{ /each }
 	</div>
 	<Footer/>
 </main>
---
+++ src/launchAPI.js
@@ -0,0 +1,15 @@
+// Launch Library API https://ll.thespacedevs.com/2.2.0/launch/upcoming/
+const apiUrl = `https://ll.thespacedevs.com/2.2.0/launch/upcoming/`;
+
+export const getData = async () => {
+  try {
+    const response = await fetch(apiUrl);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      // console.log(jsonResponse.results);
+      return jsonResponse.results;
+    }
+  } catch (error) {
+    console.log(`Uh,oh! ${error}`);
+  }
+};
