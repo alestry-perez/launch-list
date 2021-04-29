b06a6d9faf838e8ef9bed9a53632818e0b73c027
Alestry Pérez <alestry.perez@hyperisland.se>
2021-04-29 14:28:24 +0200

    commit

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 <main class='bg-gray-200 select-auto'>
 	<NavBar/>
 	<div class="flex flex-col items-center justify-center">
+		
 		<Card 
 		launchTitle='Launch Title' 
 		organization='Organization Name' 
@@ -16,7 +17,8 @@
 		time='13:45'
 		status='GO'
 		image='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.aeEolVHwf2538OOA_uWvUgHaPN%26pid%3DApi&f=1'
-		/>
+		></Card>
+		
 	</div>
 	<Footer/>
 </main>
--- src/launchAPI.svelte
+++ src/launchAPI.svelte
@@ -1,12 +1,14 @@
 <script>
-    let launchId;
+    import { onMount } from "svelte";
+  
+    export const apiURL = `https://ll.thespacedevs.com/2.0.0/launch/${launchId}`;
+    export let data = [];
 
-    const apiURL =`https://ll.thespacedevs.com/2.0.0/launch/${launchId}`;
-
+    onMount(async function() {
+        const reponse = await fetch(apiURL);
+        data = await reponse.json();
+    });
     
-	
-</script>
-
-
+  </script>
 
 
