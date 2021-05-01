38e2f2a21d8a784a1e6b71ec4f31012c14f75905
Alestry Pérez <alestry.perez@hyperisland.se>
2021-05-01 19:33:00 +0200

    commit

--- src/CountdownTimer.svelte
+++ src/CountdownTimer.svelte
@@ -1,30 +1,33 @@
 <script context="module">
+
+  let timeFromApi;
+
   // Set the date we're counting down to
-let countDownDate = new Date({ timeFromApi }).getTime();
+  const countDownDate = new Date({ timeFromApi }).getTime();
 
-// Update the count down every 1 second
-export const CountDownTime = setInterval(function () {
-  // Get today's date and time
-  let now = new Date().getTime();
+  // Update the count down every 1 second
+  export const CountDownTime = setInterval(function () {
+    // Get today's date and time
+    let now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  let distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="time"
-  document.getElementById('countdownTimer').innerHTML =
-    days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+    // Output the result in an element with id="time"
+    document.getElementById('countdownTimer').innerHTML =
+      days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
 
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(CountDownTime);
-    //document.getElementById('time').innerHTML = 'MISSION SUCCESSFUL!';
-  }
-}, 1000);
+    // If the count down is over, write some text
+    if (distance < 0) {
+      clearInterval(CountDownTime);
+      //document.getElementById('time').innerHTML = 'MISSION SUCCESSFUL!';
+    }
+  }, 1000);
 </script>
 
