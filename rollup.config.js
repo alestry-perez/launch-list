b180a0e40b3bd1608bcf179ac63f0306769e180b
Alestry Pérez <alestry.perez@hyperisland.se>
2021-05-05 02:00:10 +0200

    commit

--- rollup.config.js
+++ rollup.config.js
@@ -38,7 +38,7 @@ export default {
     sourcemap: true,
     format: 'iife',
     name: 'app',
-    file: 'docs/build/bundle.js',
+    file: 'public/build/bundle.js',
   },
   plugins: [
     svelte({
