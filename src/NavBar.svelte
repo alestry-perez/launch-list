33b672c67b983a7318383fb791f978ed1617ecee
Alestry Pérez <alestry.perez@hyperisland.se>
2021-05-06 18:32:20 +0200

    gh-pages publish

--- src/NavBar.svelte
+++
@@ -1,6 +0,0 @@
-<nav class="flex flex-col w-full px-6 py-4 text-black sm:flex-row sm:text-left sm:justify-between sm:items-baseline">
-  <div class="mb-2 sm:mb-0">
-    <h4 class="text-6xl font-bold text-center no-underline text-grey-darkest hover:text-blue-dark">🚀 Upcoming Rocket Launches</h4>
-      <!-- <button on:click={update} class='p-1 ml-5'>refresh</button> -->
-  </div>    
-</nav>
