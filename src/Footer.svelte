6b34dc1d0256cf90f0c35448d8034e3bc7e36a56
Alestry Pérez <alestry.perez@hyperisland.se>
2021-07-28 18:57:27 -0400

    commit

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="w-full p-4 text-center bg-gray-900 pin-b">
     <div class="container flex flex-col flex-wrap items-center justify-between mx-auto">
         <div class="flex mx-auto text-center text-white">
-            Made with Love by Alestry Perez 2021
+            Made with ❤️ by Alestry Pérez 2021
         </div>
     </div>
 </footer>
--- src/NavBar.svelte
+++ src/NavBar.svelte
@@ -1,6 +1,10 @@
-<nav class="flex flex-col w-full px-6 py-4 text-black sm:flex-row sm:text-left sm:justify-between sm:items-baseline">
+<script>
+  
+</script>
+
+<nav class="flex flex-col w-full p-4 px-6 py-4 text-center text-black bg-gray-900 sm:flex-row sm:text-left sm:justify-between sm:items-baseline pin-b">
   <div class="mb-2 sm:mb-0">
-    <h4 class="text-6xl font-bold text-center no-underline text-grey-darkest hover:text-blue-dark">🚀 Upcoming Rocket Launches</h4>
+    <h4 class="text-6xl font-bold text-center text-white no-underline uppercase hover:text-blue-dark">🚀 Upcoming Rocket Launches</h4>
       <!-- <button on:click={update} class='p-1 ml-5'>refresh</button> -->
   </div>    
 </nav>
